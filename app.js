d0f382a115f45f3574cdf2878015a49d5f773b92
matveimug <madis.vaher@gmail.com>
2018-05-03 00:31:47 +0300

    before exercize

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'hello world!'
+        title: 'hello world!',
+        link: 'http://google.com',
+        finishedLink: '<a href="http://google.com">google</a>'
     },
     methods: {
-        changeTitle: function(event) {
-            this.title = event.target.value;
+        sayHello: function() {
+            this.title = 'hello!';
+            return this.title;
         }
     }
 });
